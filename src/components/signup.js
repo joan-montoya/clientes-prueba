7ad9345a5bcff3bcb0662d26ff8df6f7f1b96d6a
Joan Montoya <joan15montoya@gmail.com>
2022-08-10 00:31:41 -0500

    feat: add function to connect to database

---
+++ src/components/signup.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+export default class SignUp extends Component {
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { fname, lname, email, password } = this.state;
+    console.log(fname, lname, email, password);
+    fetch("http://localhost:3000/register", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        fname,
+        email,
+        lname,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userRegister");
+      });
+  }
+  render() {
+    return (
+      <div className="cont">
+      </div>
+      
+    );
+  }
+}
